9a4299ad2f7a28005df9a278fa046b1254c937da
christopherM_harris <christopherM_harris@TO-CHECKOUT-16.midwayisd.org>
2019-11-22 10:27:27 -0600

    inventory work

--- src/com/cognitivethought/ui/InventoryBar.java
+++ src/com/cognitivethought/ui/InventoryBar.java
@@ -22,23 +22,34 @@ public class InventoryBar {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
+		for (Item item : i.getItems()) {
+			System.out.println(Item.getName(item.getId()) + "(x" + item.getQuantity() + ") at slot " + item.getPosition());
+		}
 	}
 	
 	public void render(Batch b, OrthographicCamera c) {
 		for (int i = 0; i < this.i.getItems().size(); i++) {
+			int xDisplacement = i*150;
+			float y = c.position.y+425;
+			float x = c.position.x + xDisplacement;
+			
 			b.end();
 			ShapeRenderer sp = new ShapeRenderer();
 			sp.setProjectionMatrix(c.combined);
 			sp.begin(ShapeType.Filled);
 			Gdx.gl20.glEnable(GL20.GL_BLEND_SRC_ALPHA);
 			sp.setColor(new Color(0.1f, 0.1f, 0.1f, 0.1f));
-			sp.rect(c.position.x+(i*100), c.position.y+c.viewportHeight-200, 100, 100);
+			sp.rect(x, y, 100, 100);
 			sp.setColor(new Color(1f, 1f, 1f, 1f));
-			sp.rectLine(c.position.x+(i*100), c.position.y+c.viewportHeight-200, c.position.x+(i*100) + 100, c.position.y+c.viewportHeight-100, 2);
+			sp.rectLine(x, y, x, y+100, 3);
+			sp.rectLine(x, y+100, x+100, y+100, 3);
+			sp.rectLine(x+100, y+100, x+100, y, 3);
+			sp.rectLine(x, y, x+100, y, 3);
 			Gdx.gl20.glDisable(GL20.GL_BLEND_SRC_ALPHA);
 			sp.end();
 			b.begin();
-			b.draw(Item.getTexture(this.i.getItems().get(i).getId()), c.position.x+(i*100), c.position.y+c.viewportHeight-200, 100, 100);
+			b.draw(Item.getTexture(this.i.getItems().get(i).getId()), x, y, 100, 100);
 		}
 	}
 }
